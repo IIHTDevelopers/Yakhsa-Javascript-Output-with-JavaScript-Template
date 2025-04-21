32b977816f8f21953350dbc3efc0f4402e0b437a
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-21 13:56:46 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello, world!");  // Outputting a simple message
-
-console.log("I am " + 25 + " years old.");  // Output with string concatenation
-
-console.log(`My name is John`);  // Output with template literal
