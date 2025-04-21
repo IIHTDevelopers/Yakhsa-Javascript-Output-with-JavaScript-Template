d187d4abfaefcc202baf22cd70d6ca3d4b0ff371
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-21 13:57:17 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log("Hello, world!");  // Outputting a simple message
+
+console.log("I am " + 25 + " years old.");  // Output with string concatenation
+
+console.log(`My name is John`);  // Output with template literal
